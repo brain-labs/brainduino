4375e16cde6ddbaf7ab128811a98a43d2f398981
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2016-09-16 23:29:44 -0700

    The interpreter is done

--- Brain/Brain.cpp
+++ Brain/Brain.cpp
@@ -53,18 +53,18 @@ void Brain::run(void)
     while(_code[_action] != '\0') {
         char token = _code[_action];
         switch(token) {
-            case '<': _index--; break;
-            case '>': _index++; break;
-            case '^': _index = _cells[_index]; break;
-            case '+': _cells[_index]++; break;
-            case '-': _cells[_index]--; break;
-            case '*': _cells[_index] *= _cells[_index - 1]; break;
-            case '/': _cells[_index] /= _cells[_index - 1]; break;
-            case '%': _cells[_index] %= _cells[_index - 1]; break;
-            case ',': _cells[_index] = read(); break;
-            case '.': write(char(_cells[_index])); break;
-            case '$': write(_cells[_index] / 100); break;
-            case '#': {
+            case TT_SHIFT_LEFT: _index--; break;
+            case TT_SHIFT_RIGHT: _index++; break;
+            case TT_SHIFT_UP: _index = _cells[_index]; break;
+            case TT_INCREMENT: _cells[_index]++; break;
+            case TT_DECREMENT: _cells[_index]--; break;
+            case TT_MUL: _cells[_index] *= _cells[_index - 1]; break;
+            case TT_DIV: _cells[_index] /= _cells[_index - 1]; break;
+            case TT_REM: _cells[_index] %= _cells[_index - 1]; break;
+            case TT_INPUT: _cells[_index] = read(); break;
+            case TT_OUTPUT: write(char(_cells[_index])); break;
+            case TT_FLOAT: write(_cells[_index] / 100); break;
+            case TT_DEBUG: {
                 write("Index Pointer: ", false);
                 write(_index);
                 write(" Value at Index Pointer: ", false);
@@ -72,10 +72,107 @@ void Brain::run(void)
                 write('\n');
                 break;
             }
+            case TT_BEGIN_WHILE:
+            case TT_BEGIN_FOR:
+            case TT_IF_THEN:
+            case TT_BREAK: {
+                if (_cells[_index] != 0
+                    && (token == TT_BEGIN_WHILE || token == TT_IF_THEN)) {
+                    _jumps[_indexJumps] = _action;
+                    _indexJumps++;
+                } else if (_cells[_index] > 0 && token == TT_BEGIN_FOR) {
+                    _iterations[_indexIterations] = _cells[_index];
+                    _indexIterations++;
+                    _jumps[_indexJumps] = _action;
+                    _indexJumps++;
+                } else if (token == TT_BREAK && !_indexJumps) {
+                    write("Wrong '!'");
+                } else {
+                    int loops = 1;
+                    while (loops > 0 && _code[_action] != '\0' ) {
+                        action++;
+                        if (loops == 1 
+                            && _code[_action] == TT_IF_ELSE 
+                            && token == TT_IF_THEN) {
+                            loops--;
+                            _jumps[_indexJumps] = _action;
+                            _indexJumps++;
+                        } else if (_code[_action] == TT_END_WHILE
+                                   || _code[_action] == TT_END_FOR
+                                   || _code[_action] == TT_IF_END) {
+                            loops--;
+                            if (token == TT_BREAK) {
+                                int theJump = _code[_jumps[_indexJumps - 1]];
+                                if (theJump == TT_BEGIN_WHILE) {
+                                    _indexJumps--;
+                                } else if (theJump == TT_BEGIN_FOR) {
+                                    _indexJumps--;
+                                    _indexIterations--;
+                                } else {
+                                    // it is not a real loop
+                                    _indexJumps--;
+                                    loops++;
+                                }
+                            }
+                        } else if (_code[_action] == TT_BEGIN_WHILE
+                                   || _code[_action] == TT_BEGIN_FOR
+                                   || _code[_action] == TT_IF_THEN) {
+                            loops++;
+                        }
+                    }
+                }
+
+                break;
+            }
+            case TT_END_WHILE:
+            case TT_END_FOR:
+            case TT_IF_ELSE:
+            case TT_IF_END: {
+                if (_code[_jumps[_indexJumps - 1]] == TT_IF_THEN) {
+                    if (token == TT_IF_ELSE) {
+                        int loops = 1;
+                        while (loops > 0 && _code[_action] != '\0') {
+                            action++;
+                            if (_code[_action] == TT_END_WHILE
+                                || _code[_action] == TT_END_FOR
+                                || _code[_action] == TT_IF_END) {
+                                loops--;
+                            } else if (_code[_action] == TT_BEGIN_WHILE
+                                       || _code[_action] == TT_BEGIN_FOR
+                                       || _code[_action] == TT_IF_THEN) {
+                                loops++;
+                            }
+                        }
+                    }
+                    
+                    _indexJumps--;
+                } else if (_code[_jumps[_indexJumps - 1] == TT_BEGIN_FOR]) {
+                    _iterations[_indexIterations - 1]--;
+                    if (_iterations[_indexIterations - 1] > 0) {
+                        _action = _jumps[_indexJumps - 1];
+                    } else {
+                        _indexIterations--;
+                        _indexJumps--;
+                    }
+                } else {
+                    if (_code[_jumps[_indexJumps - 1]] == TT_IF_ELSE) {
+                        _indexJumps--;
+                    } else if (_cells[_index] != 0) {
+                        _action = jumps[_indexJumps - 1];
+                    } else {
+                        _indexJumps--;
+                    }
+                }
+
+                break;
+            }
+    
             default: break;
         }
         
-        _action++;
+        if (_action != '\0') {
+            _action++;
+        }
     }
 }
 
